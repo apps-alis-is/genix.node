be704676874ddd4e1589f17178872aa653104da5
cryi <cryi@tutanota.com>
2021-11-09 18:01:25 +0100

    initial commit

---
+++ src/model.ext.lua
@@ -0,0 +1,18 @@
+am.app.set_model(
+    {
+        DAEMON_URL = "https://github.com/genix-project/genix/releases/download/v2.2.2.1/linux-binaries.zip",
+        DAEMON_CONFIGURATION = {
+            server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
+            listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
+            masternodeprivkey = am.app.get_configuration("NODE_PRIVKEY"),
+            masternode = am.app.get_configuration("NODE_PRIVKEY") and 1 or nil
+        },
+        DAEMON_NAME = "genixd",
+        CLI_NAME = "genix-cli",
+        CONF_NAME = "genix.conf",
+        CONF_SOURCE = "__btc/assets/daemon.conf",
+        SERVICE_NAME = "genixd",
+        ADD_NODES = am.app.get_configuration("ADD_NODES", { "161.43.201.255" })
+    },
+    { merge = true, overwrite = true }
+)
